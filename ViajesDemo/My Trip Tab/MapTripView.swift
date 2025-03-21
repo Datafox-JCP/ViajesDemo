9d1093e9554b58169685e19d77c1a2200a697fee
Juan Carlos Pazos <datafox@outlook.com>
2025-03-21 16:05:24 -0600

    Setup inicial

---
+++ ViajesDemo/My Trip Tab/MapTripView.swift
@@ -0,0 +1,18 @@
+//
+//  MapTripView.swift
+//  ViajesDemo
+//
+//  Created by Juan Carlos Pazos on 21/03/25.
+//
+
+import SwiftUI
+
+struct MapTripView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    MapTripView()
+}
--- ViajesDemo/ViajesDemoApp.swift
+++ ViajesDemo/ViajesDemoApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct ViajesDemoApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            StartTab()
         }
     }
 }
