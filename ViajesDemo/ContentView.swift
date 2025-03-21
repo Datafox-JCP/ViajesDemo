065a4ef19f5682122515927e3ce84bb32028de95
Juan Carlos Pazos <datafox@outlook.com>
2025-03-21 15:55:22 -0600

    Initial Commit

---
+++ ViajesDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ViajesDemo
+//
+//  Created by Juan Carlos Pazos on 21/03/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ViajesDemo/ViajesDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViajesDemoApp.swift
+//  ViajesDemo
+//
+//  Created by Juan Carlos Pazos on 21/03/25.
+//
+
+import SwiftUI
+
+@main
+struct ViajesDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
